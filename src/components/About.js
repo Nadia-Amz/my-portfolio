7f1a6eef81d6e67bcb409c12cc6348efab7e0fab
nadia-amz <nadiaamouz@gmail.com>
2024-03-14 16:56:38 +0100

    committed

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,19 @@ function about() {
   return (
     <>
     <section className="about-container">
-      <h1>About</h1>
       <div className="about-section">
         <div className="number">
         <p>01</p>
         </div>
+        <div className="paragraph">
+        <h1>About</h1>
         <p>
           I'm a front-end developer. I'm enthusiastic about crafting
           user-friendly web experiences with a strong foundation. I stay updated
           on the latest trends and am eager to contribute to innovative
           projects.
         </p>
+        </div>
       </div>
       </section>
     </>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,34 +4,60 @@ import html from "../images/html.png";
 import css from "../images/css.png";
 import js from "../images/js.png";
 import Netflix from "../videos/Netflix.mp4";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function Projects() {
   return (
     <>
-    <section className="projects-container">
-      <h1>Projects</h1>
-      <div className="ptoject-section">
-        <div className="number">
-        <p>02</p>
+      <section className="projects-container">
+        <div className="ptoject-section">
+          <div className="number">
+            <p>02</p>
+          </div>
+          <div className="paragraph">
+            <h1>Projects</h1>
+            <p>Each project is a unique piece of development.</p>
+          </div>
         </div>
-        <p>
-         Each project is a unique piece of development.
-        </p>
-      </div>
-      <div className="project-cards">
-            <div className="card">
-                <h3>Netflix</h3>
-                <p>As part of learning to code,I completed a project that involved creating a basic Netflix clone.</p>
+        <div className="project-cards">
+          <div className="card">
+            <h3>Netflix Clone</h3>
+            <p>
+              As part of learning to code,I completed a project that involved
+              creating a basic Netflix clone.
+            </p>
             <div className="languages">
-                <img className="logohtml" src={html} alt=""></img>
-                <img className="logocss" src={css} alt=""></img>
-                <img className="logojs" src={js} alt=""></img>
+              <img className="logohtml" src={html} alt=""></img>
+              <img className="logocss" src={css} alt=""></img>
+              <img className="logojs" src={js} alt=""></img>
             </div>
-                <video src={Netflix} alt=""/>
+            <video
+              className="video-section"
+              src={Netflix}
+              type="video/mp4"
+              autoPlay
+              loop
+              muted
+            />
+            <div className="icons">
+              <a href="https://github.com/Nadia-Amz/netflix" target="_blank">
+                Code
+                <FontAwesomeIcon icon={faGithub} className="icon" />
+              </a>
+              <a href="#">
+                Live Demo
+                <FontAwesomeIcon
+                  icon={faArrowUpRightFromSquare}
+                  className="icon"
+                />
+              </a>
             </div>
+          </div>
         </div>
       </section>
     </>
-  )
-}export default Projects;
+  );
+}
+export default Projects;
